e0c2e0066dc88277d39922bddbb1b002b83c940a
bustamante <kevin.bustamante@mail.novancia.fr>
2020-02-19 09:18:39 +0100

    creation User Fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        $user1 = new User();
+        $user1->setUsername("kbustamante");
+        $user1->setEmail("kevin.bustamante@mail.novancia.fr");
+        $user1->setRoles(["ROLE_ADMIN"]);
+        $password = $this->encoder->encodePassword($user1, 'kevin');
+        $user1->setPassword($password);
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setUsername("kbustam");
+        $user2->setEmail("kevin.bustamante1@orange.fr");
+        $user2->setRoles(["ROLE_USER"]);
+        $password = $this->encoder->encodePassword($user2, 'kevin');
+        $user2->setPassword($password);
+        $manager->persist($user2);
+        
         $manager->flush();
     }
 }
